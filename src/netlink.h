7b16d619d1d47915964b2e5ac56d739bbd8a7512
Diego Nehab <diego.nehab@gmail.com>
2018-02-01 16:08:49 -0200

    Add support for netlink

---
+++ src/netlink.h
@@ -0,0 +1,30 @@
+#include "timeout.h"
+#include "lua.h"
+#include "lauxlib.h"
+#include "compat.h"
+#include "lualib.h"
+#include "auxiliar.h"
+#include "socket.h"
+
+#define MAX_PAYLOAD 2048
+
+
+/*=========================================================================*\
+* .h definitions here because i'm lazy lmao 
+\*=========================================================================*/
+
+#define t_pid int
+#define t_groups int
+#define t_type int
+
+typedef struct t_netlink_{
+	t_socket fd; /*socket number (int)*/
+	t_timeout tm; /*timeout value (timeval)*/ 
+	t_pid srcpid; /*port id (int) */
+	t_pid dstpid; /*destination port id (int)*/
+	t_groups grp; /*group bitmask (int)*/
+	t_type type; /*socket type (int)*/
+}t_netlink;
+typedef t_netlink *p_netlink;
+
+int netlink_open(lua_State *L);
