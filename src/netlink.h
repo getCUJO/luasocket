d9a037e195fb971d7a099cd4e8f1bb9651166105
Marcelo Ramos <marcelo.ramos@cujo.com>
2018-02-08 09:16:23 -0200

    fixup! Add connect() to netlink sockets

--- src/netlink.h
+++ src/netlink.h
@@ -1,24 +1,37 @@
+#ifndef NETLINK_H
+#define NETLINK_H
+
+/*=========================================================================*\
+ * * NETLINK object
+ * * LuaSocket toolkit
+ * *
+ * * The netlink.h module provides LuaSocket with support for netlink protocol
+ * * (AF_NETLINK, SOCK_RAW).
+ * *
+ * * Two classes are defined: connected and unconnected. Netlink objects are
+ * * originally unconnected. They can be "connected" to a given address
+ * * with a call to the setpeername function. The same function can be used to
+ * * break the connection.
+\*=========================================================================*/
+
 #include "timeout.h"
 #include "lua.h"
 #include "socket.h"
 
 #define MAX_PAYLOAD 2048
 
+typedef int t_pid;
+typedef int t_groups;
+typedef int t_type;
 
-/*=========================================================================*\
-* .h definitions here because i'm lazy lmao 
-\*=========================================================================*/
-
-#define t_pid int
-#define t_groups int
-#define t_type int
-
-typedef struct t_netlink_{
-	t_socket fd; /*socket number (int)*/
-	t_timeout tm; /*timeout value (timeval)*/ 
-	t_pid srcpid; /*port id (int) */
-	t_type type; /*socket type (int)*/
-}t_netlink;
+typedef struct {
+    t_socket fd; 
+    t_timeout tm;  
+    t_pid srcpid; 
+    t_type type; 
+} t_netlink;
 typedef t_netlink *p_netlink;
 
 int netlink_open(lua_State *L);
+
+#endif /* NETLINK_H */
