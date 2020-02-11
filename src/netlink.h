77dbf1e3332ab721d2616fee97241512bced5947
Niko Jokinen <niko.jokinen@cujo.com>
2020-02-11 09:45:38 +0200

    UP-39: Added generic netlink socket support.

--- src/netlink.h
+++ src/netlink.h
@@ -15,6 +15,7 @@
 \*=========================================================================*/
 
 #include <linux/netlink.h>
+#include <linux/genetlink.h>
 
 #include "timeout.h"
 #include "lua.h"
@@ -31,12 +32,20 @@ struct nlmsgbuf{
     char msg[NLMSG_ALIGN(MAX_PAYLOAD)] __attribute__((aligned(NLMSG_ALIGNTO)));
 };
 
+struct nlgenmsgbuf {
+	struct nlmsghdr n;
+	struct genlmsghdr g;
+	char msg[NLMSG_ALIGN(MAX_PAYLOAD)] __attribute__((aligned(NLMSG_ALIGNTO)));
+};
+
 typedef struct {
     t_socket fd; 
     t_timeout tm;  
     t_pid srcpid; 
     t_type type; 
     struct nlmsgbuf *nlb;
+    int nl_family_id;
+    struct nlgenmsgbuf *nlgb;
 } t_netlink;
 typedef t_netlink *p_netlink;
 
